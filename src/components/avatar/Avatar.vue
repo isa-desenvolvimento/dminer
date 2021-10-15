3b02cfb5958d0660d0113ad2b58d4a12d6789a08
Andressa Novaes <isa.desenvolvimento@gmail.com>
2021-10-14 22:40:33 -0300

    feat: salvando avatat no storage

--- src/components/avatar/Avatar.vue
+++ src/components/avatar/Avatar.vue
@@ -1,13 +1,49 @@
 <template>
   <div>
-    <div id="hexagono"></div>
+    <div
+      id="hexagono"
+      :style="{ 'background-image': `url(${previewImage})` }"
+      @click="selectImage"
+    ></div>
+
+    <input ref="fileInput" type="file" @input="pickFile" class="imageInput" />
+
     <h2>{{ username }}</h2>
   </div>
 </template>
 
 <script>
 export default {
-  props: { username: { type: String, required: false } }
+  data() {
+    return {
+      previewImage: null
+    }
+  },
+  mounted() {
+    if (localStorage.avatar) {
+      this.previewImage = localStorage.avatar
+    }
+  },
+
+  props: { username: { type: String, required: false } },
+  methods: {
+    selectImage() {
+      this.$refs.fileInput.click()
+    },
+    pickFile() {
+      let input = this.$refs.fileInput
+      let file = input.files
+      if (file && file[0]) {
+        let reader = new FileReader()
+        reader.onload = (e) => {
+          this.previewImage = e.target.result
+          localStorage.avatar = e.target.result
+        }
+        reader.readAsDataURL(file[0])
+        this.$emit('input', file[0])
+      }
+    }
+  }
 }
 </script>
 
@@ -16,12 +52,11 @@ export default {
   width: 10rem;
   height: 10rem;
 
-  background: transparent
-    url('https://images.uncyc.org/pt/thumb/2/29/Mr-bean-avatar.jpg/250px-Mr-bean-avatar.jpg')
-    no-repeat;
   background-size: cover;
   background-position: center center;
   position: relative;
+
+  cursor: pointer;
 }
 #hexagono:before {
   content: '';
@@ -44,5 +79,28 @@ export default {
   border-left: var(--avatar-border-left-right);
   border-right: var(--avatar-border-left-right);
   border-top: var(--avatar-border-bottom-top);
+}
+
+/* .container {
+  width: 100%;
+  background-color: red;
+  position: fixed;
+  top: 0;
+  height: 5rem;
+  transition: 0.2s linear;
+} */
+
+.imagePreviewWrapper {
+  width: 100%;
+  height: 100%;
+  display: block;
+  cursor: pointer;
+  margin: 0 auto 30px;
+  background-size: cover;
+  background-position: center center;
+}
+
+.imageInput {
+  display: none;
 }
 </style>
