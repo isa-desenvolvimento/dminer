7ed95307966b1685368bc98813905f5f83614a26
Andressa Novaes <isa.desenvolvimento@gmail.com>
2021-11-01 23:10:18 -0300

    fix(removed): itens

--- dist/assets/Birthday.7bd108af.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.ae9b0ac4.js";import{a as o,o as s,b as a}from"./vendor.be869036.js";const t={},c={class:"about"},r=a("h1",null,"This is an about page",-1),n=[r];function _(i,d){return s(),o("div",c,n)}var l=e(t,[["render",_]]);export{l as default};
