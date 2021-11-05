1822942ab72da7a46b880b4d59b7f63b2adedc2e
Andressa Novaes <isa.desenvolvimento@gmail.com>
2021-11-05 03:53:40 -0300

    feat(search): ajusta requisicao

--- src/api/search.js
+++ src/api/search.js
@@ -1,12 +1,11 @@
 const baseURL = 'https://dminer-api.herokuapp.com/api/search'
 
 export const fetchAllSearch = async (text) => {
-  const response = await fetch(`${baseURL}-all`, {
-    method: 'POST',
+  const response = await fetch(`${baseURL}/${text}`, {
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(text)
+    }
   })
   const json = await response.json()
   return response.status === 200 ? json.data : []
