2cc7c47615fe16c73f6d62132785eaa2659e37f3
Andressa Novaes <isa.desenvolvimento@gmail.com>
2021-11-08 14:20:11 -0300

    feat(document): add fecth document

--- src/views/Document.vue
+++ src/views/Document.vue
@@ -1,8 +1,8 @@
 <template>
   <folder title="d-guide" classWrapper="folder_documents" :hasButton="false">
     <ul>
-      <li v-for="(item, key) in list" :key="key">
-        <a :href="item.link" target="_blank">
+      <li v-for="(item, key) in getDocuments" :key="key">
+        <a :href="item.content" target="_blank">
           {{ item.title }}
         </a>
       </li>
@@ -17,23 +17,6 @@ import Title from '@/components/title/Title.vue'
 import useDocument from '@/composables/useDocument'
 
 export default {
-  data() {
-    const list = [
-      {
-        title: 'teste',
-        link: 'https://br.web.img3.acsta.net/newsv7/19/05/15/17/35/0837341.jpg'
-      },
-      {
-        title: 'teste2',
-        link: 'https://1.bp.blogspot.com/-U-4jKAotb3U/XzigweF2LbI/AAAAAAAAQsY/V2LDI5Jz2go0QEERdGwvhgJHAec_f6ysACLcBGAsYHQ/s600/rick+and+morty+the+other+five.png'
-      },
-      {
-        title: 'teste2',
-        link: 'https://1.bp.blogspot.com/-U-4jKAotb3U/XzigweF2LbI/AAAAAAAAQsY/V2LDI5Jz2go0QEERdGwvhgJHAec_f6ysACLcBGAsYHQ/s600/rick+and+morty+the+other+five.png'
-      }
-    ]
-    return { list }
-  },
   setup() {
     const { getDocuments } = useDocument()
 
